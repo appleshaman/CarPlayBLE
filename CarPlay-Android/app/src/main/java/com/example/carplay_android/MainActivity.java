ea558839e1764d31f6e1d8b32264bfd9f17c9127
3of26 <76955501+appleshaman@users.noreply.github.com>
2022-11-23 15:57:38 +1300

    initial

---
+++ CarPlay-Android/app/src/main/java/com/example/carplay_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.carplay_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
